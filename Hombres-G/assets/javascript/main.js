7e5769178e0fb1a6f12c2f5ee7f9e7793dbb4562
Isaac Munguia <60395509+Mumoji83@users.noreply.github.com>
2024-04-07 18:46:11 -0600

    Agregar proyecto Hombres-G

---
+++ Hombres-G/assets/javascript/main.js
@@ -0,0 +1,45 @@
+
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function name(link) {
+
+  link.addEventListener("click", function name(ev) {
+    
+    ev.preventDefault();
+
+    let content = document.querySelector('.content');
+
+    // Quitar clases de animacion
+
+    content.classList.remove("fadeInDown");
+    content.classList.remove("animated");
+    
+    //Agregar clases de animacion
+
+    content.classList.add("fadeOutUp");
+    content.classList.add("animated");
+
+
+    setTimeout(function name() {
+      location.href = "/";  
+    }, 600);
+
+    return false;
+
+  });
+  
+});
+
+
+
+/*
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function name(icono) {
+
+  icono.classList.remove("fa-star");
+  icono.classList.add("fa-star");
+  
+});
+*/
