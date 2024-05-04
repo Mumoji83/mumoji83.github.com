9cc594f0895999f876e96315dfa59543d0b77582
Isaac Munguia <60395509+Mumoji83@users.noreply.github.com>
2024-05-03 19:05:14 -0600

    cambio 3/5/24 19:05

---
+++ RCKAST/Assets/javascript/main.js
@@ -0,0 +1,16 @@
+
+function ShowHide() {
+
+  var container = document.getElementsByClassName("cont")[0];
+
+  if (container.style.display  == "initial") {
+
+    container.style.display  = "none";
+
+  } else {
+
+    container.style.display  = "initial";
+
+  }
+  
+}
